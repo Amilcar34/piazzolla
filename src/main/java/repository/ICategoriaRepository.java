799672433068052304b6bea4a7aa500e894b35e0
eri-random <brenherrera@alumno.unlam.edu.ar>
2023-07-26 18:14:51 -0300

    list categorias

--- src/main/java/repository/ICategoriaRepository.java
+++ src/main/java/repository/ICategoriaRepository.java
@@ -2,11 +2,12 @@ package repository;
 
 import model.Categoria;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 public interface ICategoriaRepository {
-    Set<Categoria> getAllCategorias();
+    List<Categoria> getAllCategorias();
     Categoria save(Categoria categoria);
     Optional<Categoria> findById(Long idCategoria);
 }
--- src/main/java/service/ICategoriaService.java
+++ src/main/java/service/ICategoriaService.java
@@ -2,11 +2,12 @@ package service;
 
 import model.Categoria;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 public interface ICategoriaService {
-    Set<Categoria> getAllCategorias();
+    List<Categoria> getAllCategorias();
     Categoria create(Categoria categoria);
     Optional<Categoria> update(Long idCategoria, Categoria categoria);
 }
