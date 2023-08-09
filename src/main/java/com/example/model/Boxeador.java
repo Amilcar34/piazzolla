313f035d44d67f468bf4020e5eb6ea7fb3c76e7f
eri-random <brenherrera@alumno.unlam.edu.ar>
2023-08-09 11:56:44 -0300

    sugerencia: ver como sobreescribir equals y hashcode con lombok

--- src/main/java/com/example/model/Boxeador.java
+++ src/main/java/com/example/model/Boxeador.java
@@ -2,31 +2,21 @@ package com.example.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.sql.Date;
-import java.util.Objects;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class Boxeador {
     private String nombre;
     private Double peso;
     private Categoria categoria;
     private Entrenador entrenador;
+    @EqualsAndHashCode.Exclude
     private Date fechaIngreso;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Boxeador boxeador = (Boxeador) o;
-        return Objects.equals(nombre, boxeador.nombre) && Objects.equals(peso, boxeador.peso) && Objects.equals(categoria, boxeador.categoria) && Objects.equals(entrenador, boxeador.entrenador);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(nombre, peso, categoria, entrenador);
-    }
 }
